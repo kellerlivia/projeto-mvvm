ec3e0ceb0304a4d7bfaec4eb1f7c4af432a7270c
kellerlivia <rm94275@fiap.com.br>
2023-07-06 21:08:52 -0300

    base do projeto

---
+++ mvvm/mvvm/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by Livia Keller on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
